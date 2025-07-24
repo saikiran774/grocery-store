61fd833b905dcd16dadac10a4f16d8d3beaf2fa2
Lavudya saikiran <officialsai@74gmail.com>
2025-07-24 22:11:41 +0530

    commit

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,8 +1,16 @@
+// import multer from "multer";
+// const storage = multer.diskStorage({
+//   destination: "uploads",
+//   filename: (req, file, cb) => {
+//     return cb(null, `${Date.now()}${file.originalname}`);
+//   },
+// });
+//   export const upload = multer({ storage: storage });
+ 
+// backend/config/multer.js ✅
 import multer from "multer";
-const storage = multer.diskStorage({
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`);
-  },
-});
-export const upload = multer({ storage: storage });
+
+const storage = multer.memoryStorage(); // Stores file in RAM (no disk write)
+export const upload = multer({ storage });
+
+
